096638543c467287312d473d83c999e186514b28
Valentin Michelle <valentinmichelle16@gmail.com>
2025-07-08 19:25:59 -0300

    Arreglando errores finales

--- includes/header.php
+++ includes/header.php
@@ -8,15 +8,15 @@
 
     <title>EEST N.º 1 – <?= $pageTitle ?? 'Inicio'; ?></title>
 
-    <link rel="stylesheet" href="/src/styles/styles.css">
-    <link rel="icon" href="/src/assets/img/logo.webp" type="image/webp">
+    <link rel="stylesheet" href="../src/styles/styles.css">
+    <link rel="icon" href="../src/assets/img/logo.webp" type="image/webp">
     <script src="https://kit.fontawesome.com/d0c30b1127.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="<?= $pageSlug ?? 'page--home'; ?>">
 <header class="header">
     <div class="header__brand">
-        <img class="header__logo" src="/src/assets/img/logo.webp" alt="Logo EEST N.º 1">
+        <img class="header__logo" src="../src/assets/img/logo.webp" alt="Logo EEST N.º 1">
         <span class="header__title">EESTN1</span>
     </div>
 
@@ -24,21 +24,21 @@
 
     <nav class="nav" id="mainNav" aria-label="Menú principal">
         <ul class="nav__list">
-            <li class="nav__item"><a href="/public/index.php">Inicio</a></li>
-            <li class="nav__item"><a href="/public/historia.php">Historia</a></li>
+            <li class="nav__item"><a href="../public/index.php">Inicio</a></li>
+            <li class="nav__item"><a href="../public/historia.php">Historia</a></li>
 
             <li class="nav__item nav__item--has-dropdown">
                 <a href="#">Especialidades</a>
                 <ul class="nav__dropdown">
-                    <li><a href="/public/ciclo-basico.php">Ciclo básico</a></li>
-                    <li><a href="/public/informatica.php">Informática</a></li>
-                    <li><a href="/public/programacion.php">Programación</a></li>
-                    <li><a href="/public/mmo.php">M.M.O.</a></li>
+                    <li><a href="../public/ciclo-basico.php">Ciclo básico</a></li>
+                    <li><a href="../public/informatica.php">Informática</a></li>
+                    <li><a href="../public/programacion.php">Programación</a></li>
+                    <li><a href="../public/mmo.php">M.M.O.</a></li>
                 </ul>
             </li>
 
-            <li class="nav__item"><a href="/public/contacto.php">Contacto</a></li>
-            <li class="nav__item"><a href="/public/inscripcion.php">Inscripción</a></li>
+            <li class="nav__item"><a href="../public/contacto.php">Contacto</a></li>
+            <li class="nav__item"><a href="../public/inscripcion.php">Inscripción</a></li>
             <li class="nav__item"><a href="http://eestn1tfeb.blogspot.com/" target="_blank">Blog</a></li>
 
             <!-- Instagram -->
